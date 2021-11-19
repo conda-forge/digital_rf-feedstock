908b37df13c2d6ad8d3380013be9e999468e04cf
Ryan Volz <rvolz@mit.edu>
2021-11-19 10:55:39 -0500

    Try explicitly setting the Python executable for CMake.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,7 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DDRF_INSTALL_PREFIX_PYTHON=$PREFIX \
+    -DPython_EXECUTABLE=$PYTHON \
     ..
 cmake --build .
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
