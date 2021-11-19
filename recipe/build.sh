3f1b5c40b28c73253bcb477726ea937f4f8e4233
Ryan Volz <rvolz@mit.edu>
2021-11-19 09:55:08 -0500

    Try using the default findPython CMake variables again.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,6 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DDRF_INSTALL_PREFIX_PYTHON=$PREFIX \
-    -DPython_FIND_FRAMEWORK=NEVER \
-    -DPython_FIND_STRATEGY=LOCATION \
     ..
 cmake --build .
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
